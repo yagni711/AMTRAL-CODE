fa5f4d4bd11b2438ee290b82f982a42318c9a12c
jinal <drjinalpatel@gmail.com>
2024-01-30 19:54:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from "./footer/footer.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, RouterModule,HeaderComponent, FooterComponent, DashboardComponent,LoginComponent]
 })
 export class AppComponent {
   title = 'amtrak';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+export const routes: Routes = [{
+    path: 'loginpage',
+    component: LoginComponent
+}, {
+    path: '',
+    component: DashboardComponent
+}];
